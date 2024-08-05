69f2f06ef2649df8dde38140c9cd360275543ed6
Miguel Placido Felix <164278747+Miguelfelix16@users.noreply.github.com>
2024-08-05 23:00:43 +0000

    05/08/24

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const meuNome = "Miguel";
 const idade = 17;
 const altura = 1.65;
-const massa = 
+const massa = 65;
+const cidade = "sao jose dos pinhas";
+
 
 console.log(meuNome);
 console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
